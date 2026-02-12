0248a02b7180a94b0eef71d9db393083ce8ab2f8
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-02-12 09:11:38 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let user = ' [' + prompt('Enter a comma-seperated list of the numbers that The Keymaster gave you') + ']'
+let a = 4;
+let b = 8;
+let c = 15;
+let d = 20;
+let e = 48;
+let f = 100;
+
+let result = String(b - a) + String(c - b) + String(d - c) + String(e - d) + String(f - e);
+document.getElementById('result').innerHTML = result;
