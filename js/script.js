99a911ec6137b0bf7749e145c05ca57068eea3d3
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-03-04 11:01:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 async function readResponses() {
-  let x = await fetch('data/keymasterResponses.json');
+  let x = await fetch('../data/keymasterResponses.json');
   let y = await x.json();
   y.forEach((element) => console.log(element));
 }
 
-/*let user = ' [' + prompt('Enter a comma-seperated list of the numbers that The Keymaster gave you') + ']'
-let a = 4;
-let b = 8;
-let c = 15;
-let d = 20;
-let e = 48;
-let f = 100;
-
-let result = String(b - a) + String(c - b) + String(d - c) + String(e - d) + String(f - e);
-document.getElementById('result').innerHTML = result;*/
+onload = await readResponses();
