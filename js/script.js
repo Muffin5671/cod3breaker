0f9b3460dda3e3a6e0852f3830fbe87d0c5b9524
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-03-05 22:51:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 let sfx = new Audio('sounds/achievement.mp3');
 
+function createElement(tag, content, style) {
+  let element = document.createElement(tag);
+  element.innerHTML = content;
+  if (!(style == undefined)) {
+    element.style = style;
+  }
+  document.body.appendChild(element);
+}
+
 async function readResponses() {
   let x = await fetch('data/keymasterResponses.json');
   let y = await x.json();
