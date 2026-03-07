59144ce47259d42137a606e8a69c06116e0915a3
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-03-07 13:54:06 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+// setup variables
 let music = new Audio('sounds/secretLoop.mp3');
 let sfx = new Audio('sounds/achievement.mp3');
 
+let response;
+let kmResponseNum;
+
+// when you enter this page for the first time
 if (localStorage.vosSettings == undefined) {
   localStorage.vosSettings = '{"userName": "", "audio": false}'
 }
 
+// plays audio on page click
 function playAudio() {
   document.body.addEventListener("click", audioCheck)
 }
 
+// checks if audio option is on/off
 function audioCheck() {
   if (JSON.parse(localStorage.vosSettings).audio) {
     music.loop = true;
@@ -16,30 +23,48 @@ function audioCheck() {
   }
 }
 
+// keymaster responses are in an external JSON
 async function readResponses() {
+  
   let url = 'data/keymasterResponses.json';
+  
   const options = {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
   }
+  
   try {
     let data = await fetch(url, options);
-    let response = await data.json();
+    response = await data.json();
+    kmResponseNum = Math.floor(Math.random() * response.length);
     response.forEach((element) => console.log(element));
+    return response;
   } catch (err) {
     document.getElementById('keymasterResponse').innerHTML = 'Something went wrong...';
   }
 }
 
+// fetches keymaster responses when page loads
 window.onload = readResponses();
 
+// get ready for probably the biggest function in this script file
+function nextMessage() {
+  document.getElementById('keymasterResponse').innerHTML = response[kmResponseNum].message;
+  document.getElementById('keymasterResponse').style.color = response[kmResponseNum].color;
+  kmResponseNum++;
+}
+
 function achievement(name, cubeID) {
-  sfx.play();
+  
+  if (JSON.parse(localStorage.vosSettings).audio) {
+    sfx.play();
+  }
+  
   let element = document.createElement('div');
-  element.id = 'achPopup'
-  element.className = 'brownbox'
+  element.id = 'achPopup';
+  element.className = 'brownbox';
   let achName = document.createElement('p');
   achName.id = 'achName';
   achName.innerText = name;
